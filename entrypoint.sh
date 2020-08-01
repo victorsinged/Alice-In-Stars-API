e57dba799321ed2de6bce39213becb5f3b403fd8
Ale <victor.brito@vindi.com.br>
2020-08-01 19:12:27 -0300

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 # Checks if the dependencies listed in Gemfile are satisfied by currently installed gems
 bundle clean --force
 
+# Remove a potentially pre-existing server.pid for Rails.
+rm -rf /app/tmp/pids/server.pid
+
 # Check whether or not gems are installed, and install it case not installed.
 bundle check || bundle install --jobs=$(nproc) --retry=5
 
