454a277db261d27a13d7d96ddfddbfc5bdd9a486
ale <kyotoshikaranosinged@protonmail.ch>
2021-02-14 17:10:55 -0300

    add nasa api handler service as suggested * from code review

---
+++ app/services/nasa_api_handler.rb
@@ -0,0 +1,23 @@
+class NasaApiHandler
+  BASE_URL = ENV.fetch('NASA_API_BASE_URL').freeze
+  NASA_API_KEY = ENV.fetch('NASA_API_KEY').freeze
+  NEOWS_URL = "#{BASE_URL}/neo/rest/v1/feed".freeze
+
+  def initialize(http_client)
+    @http_client = http_client
+  end
+
+  def catch_neows(start_date:, end_date:)
+    http_client_class = @http_client.fetch(:http_client)
+    http_client_class.get_neows(
+      uri: NEOWS_URL,
+      api_key: NASA_API_KEY,
+      start_date: start_date,
+      end_date: end_date
+    )
+  end
+
+  private
+
+  attr_reader :http_client
+end
