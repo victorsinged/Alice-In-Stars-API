26ebbfd6c7a5648f5d8653f0f30d0cda76d94e37
ale <kyotoshikaranosinged@protonmail.ch>
2021-02-16 15:44:35 -0500

    change args from constructor and remove fetch

--- app/services/nasa_api_service.rb
+++ app/services/nasa_api_service.rb
@@ -1,14 +1,13 @@
 class NasaApiService
-  def initialize(nasa_api_handler)
+  def initialize(nasa_api_handler:)
     @nasa_api_handler = nasa_api_handler
   end
 
   def catch_neows_by_date_range(start_date:, end_date:)
     start_date_or_today = start_date ||= Date.today
     end_date_or_today = end_date ||= Date.today
-    nasa_api_handler_class = @nasa_api_handler.fetch(:nasa_api_handler)
 
-    nasa_api_handler_class.catch_neows(
+    nasa_api_handler.catch_neows(
       start_date: start_date_or_today,
       end_date: end_date_or_today
     )
